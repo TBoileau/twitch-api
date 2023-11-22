8a3390500d5c5f4cfd81b0a1870bb41d666319ab
TBoileau <t-boileau@email.com>
2023-11-22 12:15:39 +0100

    feat: add ghost properties

--- src/Api/TwitchApiInterface.php
+++ src/Api/TwitchApiInterface.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace TBoileau\TwitchApi\Api;
 
 use TBoileau\TwitchApi\Api\Endpoint\AbstractOperations;
+use TBoileau\TwitchApi\Api\Endpoint\Bits\BitsOperations;
 use TBoileau\TwitchApi\Api\Endpoint\Channel\ChannelOperations;
+use TBoileau\TwitchApi\Api\Endpoint\Subscriptions\SubscriptionsOperations;
 
 /**
- * @property ChannelOperations $channel
+ * @property ChannelOperations $Channel
+ * @property SubscriptionsOperations $Subscriptions
+ * @property BitsOperations $Bits
  */
 interface TwitchApiInterface
 {
